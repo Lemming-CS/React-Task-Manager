02ef9eaddfd1d0c9342623ff1d956083095581f1
Lemming-CS <220599209+Lemming-CS@users.noreply.github.com>
2026-03-13 23:09:47 +0600

    Add mobile friendly design for login/register page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,24 +1,33 @@
 import styles from "./static/Login.module.css";
 import { useState } from "react";
-import { signInWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import {
+  signInWithEmailAndPassword,
+  GoogleAuthProvider,
+  signInWithPopup,
+} from "firebase/auth";
 import googleIMG from "../assets/google.png";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
-import {db, auth} from "../firebase/config.js";
+import { db, auth } from "../firebase/config.js";
 import { doc, getDoc, setDoc } from "firebase/firestore";
 import { useStore } from "../store/useStore.js";
 import Header from "../components/Header.jsx";
 import getFirestoreInfo from "../store/getFirestoreInfo";
 
 function Login() {
+  
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
   const [errorMessage, setErrorMessage] = useState("");
-  const setUser = useStore( state => state.setUser);
+  const setUser = useStore((state) => state.setUser);
   const handleLogin = async () => {
     try {
-      const userCredential = await signInWithEmailAndPassword(auth, email, password);
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password,
+      );
       const user = userCredential.user;
       if (!user.emailVerified) {
         setErrorMessage("Please verify your email before logging in.");
@@ -26,28 +35,22 @@ function Login() {
       }
       await getFirestoreInfo(user);
       navigate("/");
-    }
-      catch (error) {
+    } catch (error) {
       if (error.code === "auth/invalid-credential") {
-          setErrorMessage("Invalid password or Email");
-        }
-      else if (error.code === "auth/missing-password") {
+        setErrorMessage("Invalid password or Email");
+      } else if (error.code === "auth/missing-password") {
         setErrorMessage("Missing password");
-      }
-      else if (error.code === "auth/invalid-email") {
+      } else if (error.code === "auth/invalid-email") {
         setErrorMessage("Missing Email");
-      }
-      else {
+      } else {
         setErrorMessage(error.code);
-        }
       }
-  }
-
+    }
+  };
 
   const handleGoogleLogin = async () => {
     const provider = new GoogleAuthProvider();
     try {
-      
       const userCredential = await signInWithPopup(auth, provider);
       const user = userCredential.user;
       const userDocRef = doc(db, "users", user.uid);
@@ -65,46 +68,44 @@ function Login() {
         };
         await setDoc(userDocRef, newUser);
         setUser(newUser);
-      }
-      else {
+      } else {
         await getFirestoreInfo(user);
       }
       navigate("/");
-    }
-    catch (error) {
-        setErrorMessage(error.message);
+    } catch (error) {
+      setErrorMessage(error.message);
     }
   };
 
   return (
     <>
-    <Header/>
-    <div className={styles.login}>
-      <h1>Login</h1>
-      <input
-        type="email"
-        placeholder="Email"
-        value={email}
-        onChange={e => setEmail(e.target.value)}
-      />
-      <input
-        type="password"
-        placeholder="Password"
-        value={password}
-        onChange={e => setPassword(e.target.value)}
-      />
-      <span className="error">{errorMessage}</span>
-      <button onClick={handleLogin}>Login</button>
-      <button className={styles.googleBtn} onClick={handleGoogleLogin}>
-        <img src={googleIMG} alt="Google Logo" />
-        <span>Continue with Google</span>
-      </button>
-      <Link to="/register" id="redirect">
-        Don't have an account?
-      </Link>
-
-    </div>
-  </>
-  )}
+      <Header />
+      <div className={styles.login}>
+        <h1>Login</h1>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <span className="error">{errorMessage}</span>
+        <button onClick={handleLogin}>Login</button>
+        <button className={styles.googleBtn} onClick={handleGoogleLogin}>
+          <img src={googleIMG} alt="Google Logo" />
+          <span>Continue with Google</span>
+        </button>
+        <Link to="/register" id="redirect">
+          Don't have an account?
+        </Link>
+      </div>
+    </>
+  );
+}
 
 export default Login;
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,4 +1,4 @@
-import styles from "./static/Register.module.css"
+import styles from "./static/Register.module.css";
 import { useState } from "react";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../firebase/config";
@@ -8,98 +8,107 @@ import { db } from "../firebase/config";
 import { doc, setDoc, serverTimestamp } from "firebase/firestore";
 import { sendEmailVerification } from "firebase/auth";
 import Header from "../components/Header";
+
 function Register() {
+  
+  const [email, setEmail] = useState("");
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+  const [errorMessage, setErrorMessage] = useState("");
+  const [showModal, setShowModal] = useState(false);
 
-    const [email, setEmail] = useState("");
-    const [userName, setUserName] = useState("");
-    const [password, setPassword] = useState("");
-    const navigate = useNavigate();
-    const [errorMessage, setErrorMessage] = useState("");
-    const [showModal, setShowModal] = useState(false);
-
-    const handleRegister = async () => {
-        if (!userName) {
-          setErrorMessage("Username shouldn't be empty!");
-          return;
-        }
-        else if (userName.trim().length < 3 || userName.trim().length > 20) {
-          setErrorMessage("Username must have length between 3 and 20 characters!")
-          return;
-        }
-        try {
-            const userCredential = await createUserWithEmailAndPassword(auth, email, password);
-            const user = userCredential.user;
-            sendEmailVerification(user);
-
-            await setDoc(doc(db, "users", user.uid), {
-              uid: user.uid,
-              username: userName.trim(),
-              email: user.email,
-              bio: "",
-              profilePicture: null,
-              usernameLower: userName.trim().toLowerCase(),
-              createdAt: serverTimestamp()
-            });
-            setShowModal(true)
-        }
-        catch (error) {
-          if (error.code === "auth/weak-password") {
-            setErrorMessage("Password must be at least 6 characters.");
-          } 
-          else if (error.code === "auth/email-already-in-use") {
-            setErrorMessage("This email is already registered.");
-          } 
-          else if (error.code === "auth/password-does-not-meet-requirements") {
-            setErrorMessage("Password must be 6 characters long, have one lowercase and numeric character"); 
-          }
-          else {
-            setErrorMessage(error.code);
-          }
-        }
+  const handleRegister = async () => {
+    if (!userName) {
+      setErrorMessage("Username shouldn't be empty!");
+      return;
+    } else if (userName.trim().length < 3 || userName.trim().length > 20) {
+      setErrorMessage("Username must have length between 3 and 20 characters!");
+      return;
     }
+    try {
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password,
+      );
+      const user = userCredential.user;
+      sendEmailVerification(user);
 
-  return <>
-  <Header />
-    <div className={styles.register}>
-      <h1>Register</h1>
-      <input
-        type="input"
-        placeholder="Username"
-        value={userName}
-        onChange={e => setUserName(e.target.value)}
-      />
-      <input
-        type="email"
-        placeholder="Email"
-        value={email}
-        onChange={e => setEmail(e.target.value)}
-      />
-      <input
-        type="password"
-        placeholder="Password"
-        value={password}
-        onChange={e => setPassword(e.target.value)}
-      />
-      <span className="error">{errorMessage}</span>
-      <button onClick={handleRegister}>Register</button>
-      <Link to="/login" id="redirect">Login Page</Link>
-    </div>
+      await setDoc(doc(db, "users", user.uid), {
+        uid: user.uid,
+        username: userName.trim(),
+        email: user.email,
+        bio: "",
+        profilePicture: null,
+        usernameLower: userName.trim().toLowerCase(),
+        createdAt: serverTimestamp(),
+      });
+      setShowModal(true);
+    } catch (error) {
+      if (error.code === "auth/weak-password") {
+        setErrorMessage("Password must be at least 6 characters.");
+      } else if (error.code === "auth/email-already-in-use") {
+        setErrorMessage("This email is already registered.");
+      } else if (error.code === "auth/password-does-not-meet-requirements") {
+        setErrorMessage(
+          "Password must be 6 characters long, have one lowercase and numeric character",
+        );
+      } else {
+        setErrorMessage(error.code);
+      }
+    }
+  };
+
+  return (
+    <>
+      <Header />
+      <div className={styles.register}>
+        <h1>Register</h1>
+        <input
+          type="input"
+          placeholder="Username"
+          value={userName}
+          onChange={(e) => setUserName(e.target.value)}
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <span className="error">{errorMessage}</span>
+        <button onClick={handleRegister}>Register</button>
+        <Link to="/login" id="redirect">
+          Login Page
+        </Link>
+      </div>
       {showModal && (
         <div className={styles.modalOverlay}>
           <div className={styles.modal}>
             <h2>Email Verification</h2>
             <p>
-              We sent a verification email. Please verify your account before logging in. Check the spam folder if you didn't get it.
+              We sent a verification email. Please verify your account before
+              logging in. Check the spam folder if you didn't get it.
             </p>
-            <button onClick={() => {
-              setShowModal(false);
-              navigate("/login");
-            }}>
+            <button
+              onClick={() => {
+                setShowModal(false);
+                navigate("/login");
+              }}
+            >
               OK
             </button>
           </div>
         </div>
       )}
     </>
+  );
 }
 export default Register;
